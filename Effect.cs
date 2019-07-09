8f91e4c21cac41e41b0f5aca7928608a162035ed
HyeonseongBae <48095716+HyeonseongBae@users.noreply.github.com>
2019-07-09 23:49:49 +0900

    Add files via upload

---
+++ Effect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    public Transform player;
+    public Transform aura;
+
+    public ParticleSystem redPs;
+    public ParticleSystem bluePs;
+
+    AudioSource redAudio;
+    AudioSource blueAudio;
+
+    public bool overlap = false;
+
+    Vector3 auraPos = new Vector3();
+
+    private void Start()
+    {
+       // player = GameObject.Find("Player").transform;
+         aura = GameObject.Find("Aura").transform;
+        //redPs = GameObject.Find("RedEffect").GetComponent<ParticleSystem>();
+        //bluePs = GameObject.Find("BlueEffect").GetComponent<ParticleSystem>();
+
+        redAudio = redPs.GetComponent<AudioSource>();
+        blueAudio = bluePs.GetComponent<AudioSource>();
+
+        redPs.Stop();
+        bluePs.Stop();
+
+        //Instantiate(aura,new Vector3(10,0,0),Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetMouseButtonDown(0))
+        {
+            NoteEffect();
+        }
+
+        aura.transform.position = new Vector3(CreateTile.S.que_TileObj.Peek().transform.position.x, 0.5f, CreateTile.S.que_TileObj.Peek().transform.position.z);
+    }
+
+    void NoteEffect()
+    {
+        float distance = Vector3.Distance(player.position, aura.transform.position); // 거리 계산
+
+        Debug.Log(distance);
+
+        if (distance <= 1.5 && distance >= 0) //distance <= 0.5 && distance >= 0
+        {
+            bluePs.Play();
+            blueAudio.Play();
+        }
+        else if (distance <= 2.5 && distance > 1.5) //distance <= 1.5 && distance > 0.25
+        {
+            redPs.Play();
+            redAudio.Play();
+        }
+    }
+}
---
+++ Spectrum.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spectrum : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public int index;
+    public float power;
+    public float cycleTime;
+    float[] current = new float[64];
+    float curTime = 0;
+
+    private int rotNum = 0;
+
+    
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        SearchSpectrum();
+
+    }
+
+    private void Start()
+    {
+        ChangeMusic();
+    }
+
+    private void Update()
+    {
+        if (curTime > cycleTime)
+        {
+            SearchSpectrum();
+            curTime = 0;
+        }
+        curTime += Time.deltaTime;
+    }
+
+
+    void SearchSpectrum()
+    {
+        audioSource.GetSpectrumData(current, 0, FFTWindow.Rectangular);
+        //print(current[index]);
+        if (current[index] >= power)
+        {
+            int rand = Random.Range(-1, 2);
+            switch (rand)
+            {
+                case 0:
+                    break;
+                case -1:
+                    if (rotNum <= -2)
+                    {
+                        CreateTile.S.Tile_Turn(RIGHT_LEFT.TYPE_RIGHT);
+                        rotNum++;
+                    }
+                    else
+                    {
+                        CreateTile.S.Tile_Turn(RIGHT_LEFT.TYPE_LEFT);
+                        rotNum--;
+                    }
+                    break;
+                case 1:
+                    if (rotNum >= 2)
+                    {
+                        CreateTile.S.Tile_Turn(RIGHT_LEFT.TYPE_LEFT);
+                        rotNum--;
+                    }
+                    else
+                    {
+                        CreateTile.S.Tile_Turn(RIGHT_LEFT.TYPE_RIGHT);
+                        rotNum++;
+                    }
+                    break;
+                default:
+                    Debug.Log("Spectrum Tile_Ture Error!!");
+                    return;
+            }
+        }
+    }
+
+
+    void ChangeMusic()
+    {
+        if (StartSceneManager.S.Type == StartSceneManager.MusicType.A)
+        {
+            audioSource.clip = StartSceneManager.S.AM;
+            audioSource.Play();
+        }
+        if (StartSceneManager.S.Type == StartSceneManager.MusicType.B)
+        {
+            audioSource.clip = StartSceneManager.S.BM;
+            audioSource.Play();
+        }
+        if (StartSceneManager.S.Type == StartSceneManager.MusicType.C)
+        {
+            audioSource.clip = StartSceneManager.S.CM;
+            audioSource.Play();
+        }
+        if (StartSceneManager.S.Type == StartSceneManager.MusicType.D)
+        {
+            audioSource.clip = StartSceneManager.S.DM;
+            audioSource.Play();
+        }
+    }
+
+
+    }
